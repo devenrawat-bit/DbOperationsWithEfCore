618428098610a5bcf47983e2ef631a94e08f9c58
devenrawat-bit <rawatdeven579@gmail.com>
2025-10-27 14:42:32 +0530

    added the db context and other setting related to it

--- DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Data/AppDbContext.cs
@@ -4,9 +4,9 @@ namespace DbOperationsWithEfCoreApp.Data
 {
     public class AppDbContext:DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
     }
 }
--- DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
+++ DbOperationsWithEfCoreApp/DbOperationsWithEfCoreApp/Program.cs
@@ -1,4 +1,6 @@
+using DbOperationsWithEfCoreApp.Data;
 
+using Microsoft.EntityFrameworkCore;
 namespace DbOperationsWithEfCoreApp
 {
     public class Program
@@ -6,9 +8,10 @@ namespace DbOperationsWithEfCoreApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb")));
+            //Add services to the container.
+           builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -25,7 +28,6 @@ namespace DbOperationsWithEfCoreApp
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
